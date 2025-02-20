3164ecf5315bbf8f9b318ff7ba8b6b1f9a0bfca4
hayas1 <h4ystack@gmail.com>
2025-02-20 15:08:53 +0000

    coalesced

--- src/lib.rs
+++ src/lib.rs
@@ -13,6 +13,46 @@ impl<T, E> Coalesce for Result<T, E> {
     }
 }
 
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Default, Hash)]
+pub struct Coalesced<C> {
+    priority: std::collections::VecDeque<C>,
+}
+impl<C> Coalesced<C> {
+    pub fn new() -> Self {
+        Self {
+            priority: Default::default(),
+        }
+    }
+    pub fn push(&mut self, coalesce: C) {
+        self.priority.push_back(coalesce);
+    }
+    pub fn pop(&mut self) -> Option<C> {
+        self.priority.pop_back()
+    }
+    pub fn push_front(&mut self, coalesce: C) {
+        self.priority.push_front(coalesce);
+    }
+    pub fn pop_front(&mut self) -> Option<C> {
+        self.priority.pop_front()
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.priority.is_empty()
+    }
+    pub fn peek(&self) -> Option<&C> {
+        self.priority.back()
+    }
+    pub fn peek_mut(&mut self) -> Option<&mut C> {
+        self.priority.back_mut()
+    }
+    pub fn peek_front(&self) -> Option<&C> {
+        self.priority.front()
+    }
+    pub fn peek_front_mut(&mut self) -> Option<&mut C> {
+        self.priority.front_mut()
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
