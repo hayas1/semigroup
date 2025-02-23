fd5bdd835ae38f5aaad7970d25f5b41d568ef300
hayas1 <h4ystack@gmail.com>
2025-02-23 02:01:28 +0000

    prior

--- src/lib.rs
+++ src/lib.rs
@@ -22,105 +22,46 @@ impl<T> CoalescePosterior for Option<T> {
     }
 }
 
-pub trait Coalesce: CoalescePrior + CoalescePosterior {
-    fn prior(self, other: Self) -> Self
-    where
-        Self: Sized,
-    {
-        if <Self as CoalescePrior>::is_other(&self, &other) {
-            other
-        } else {
-            self
-        }
-    }
-
-    fn posterior(self, other: Self) -> Self
-    where
-        Self: Sized,
-    {
-        if <Self as CoalescePosterior>::is_other(&self, &other) {
-            other
-        } else {
-            self
-        }
-    }
-}
-impl<T> Coalesce for Option<T> {}
-
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Default, Hash)]
 pub struct Coalesced<C> {
-    history: Vec<C>,
-    current: C,
+    priority: Vec<C>,
+    accessor: usize,
 }
 
-impl<C: CoalescePrior> CoalescePrior for Coalesced<C> {
-    fn is_other(&self, other: &Self) -> bool {
-        <C as CoalescePrior>::is_other(&self.current, &other.current)
-    }
-}
-impl<C: CoalescePosterior> CoalescePosterior for Coalesced<C> {
-    fn is_other(&self, other: &Self) -> bool {
-        <C as CoalescePosterior>::is_other(&self.current, &other.current)
-    }
-}
-impl<C: Coalesce> Coalesce for Coalesced<C> {
-    fn prior(self, other: Self) -> Self {
-        let (low, high) = if <Self as CoalescePrior>::is_other(&self, &other) {
-            (self, other)
-        } else {
-            (other, self)
-        };
-        let (mut history, current) = (low.history, high.current);
-        history.extend(high.history);
-        history.push(low.current);
-        Coalesced { history, current }
-    }
-
-    fn posterior(self, other: Self) -> Self {
-        let (low, high) = if <Self as CoalescePosterior>::is_other(&self, &other) {
-            (self, other)
-        } else {
-            (other, self)
-        };
-        let (mut history, current) = (low.history, high.current);
-        history.extend(high.history);
-        history.push(low.current);
-        Coalesced { history, current }
-    }
-}
 impl<C> std::ops::Deref for Coalesced<C> {
     type Target = C;
     fn deref(&self) -> &Self::Target {
-        &self.current
+        &self.priority[self.accessor]
     }
 }
 impl<C> std::ops::DerefMut for Coalesced<C> {
     fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.current
+        &mut self.priority[self.accessor]
     }
 }
 impl<C> Coalesced<C> {
     pub fn new(coalesce: C) -> Self {
         Self {
-            history: Vec::new(),
-            current: coalesce,
+            priority: vec![coalesce],
+            accessor: 0,
         }
+    }
+
+    pub fn prior(mut self, other: Self) -> Self {
+        self.priority.extend(other.priority);
+        self.accessor += other.accessor;
+        self
+    }
+    pub fn posterior(self, mut other: Self) -> Self {
+        other.priority.extend(self.priority);
+        other.accessor += self.accessor;
+        other
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn test_coalesce_trait() {
-        let none = None;
-        let some1 = Some(1);
-        let some2 = Some(2);
-
-        assert_eq!(none.prior(some1).prior(some2), some1);
-        assert_eq!(none.posterior(some1).posterior(some2), some2);
-    }
 
     #[test]
     fn test_coalesced_prior_history() {
@@ -130,7 +71,10 @@ mod tests {
 
         let config = from_file.prior(from_env).prior(from_cli);
         assert_eq!(config.unwrap(), "file");
-        assert_eq!(config.history, vec![Some("env"), Some("cli")]);
+        assert_eq!(
+            config.priority,
+            vec![Some("file"), Some("env"), Some("cli")]
+        );
     }
 
     #[test]
@@ -141,6 +85,9 @@ mod tests {
 
         let config = from_file.posterior(from_env).posterior(from_cli);
         assert_eq!(config.unwrap(), "cli");
-        assert_eq!(config.history, vec![Some("file"), Some("env")]);
+        assert_eq!(
+            config.priority,
+            vec![Some("file"), Some("env"), Some("cli")]
+        );
     }
 }
--- tests/basic.rs
+++ tests/basic.rs
@@ -1,4 +1,4 @@
-use coalesced::{Coalesce, Coalesced};
+use coalesced::Coalesced;
 
 pub struct Config<'a> {
     pub name: &'a str,
@@ -9,6 +9,6 @@ pub fn main() {
     let from_env = Coalesced::new(Some(Config { name: "env" }));
     let from_cli = Coalesced::new(Some(Config { name: "cli" }));
 
-    let config = from_file.posterior(from_env).posterior(from_cli);
+    let config = from_file.prior(from_env).prior(from_cli);
     assert_eq!(config.as_ref().unwrap().name, "cli");
 }
