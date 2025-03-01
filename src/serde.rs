ef4d49126212e44169f49fb20eda4d7c6edf3b30
hayas1 <h4ystack@gmail.com>
2025-03-01 05:59:27 +0000

    none

--- src/serde.rs
+++ src/serde.rs
@@ -88,4 +88,49 @@ mod tests {
 
         assert_eq!(config.number.unwrap(), 100);
     }
+
+    #[test]
+    fn test_coalesced_serialize_with_none() {
+        let file = Config {
+            name: "file",
+            number: Coalesced::new_prior(Some(1)),
+        };
+        let env = Config {
+            name: "env",
+            number: Coalesced::new_prior(Some(10)),
+        };
+        let cli = Config {
+            name: "cli",
+            number: Coalesced::new_prior(None),
+        };
+
+        let config = Config {
+            name: "config",
+            number: file
+                .number
+                .extend_prior(env.number)
+                .extend_prior(cli.number)
+                .into_single(),
+        };
+
+        let serialized = serde_json::to_string(&config).unwrap();
+        assert_eq!(serialized, r#"{"name":"config","number":10}"#);
+    }
+    #[test]
+    fn test_coalesced_deserialize_with() {
+        let file: Config = serde_json::from_str(r#"{"name":"file","number":1}"#).unwrap();
+        let env: Config = serde_json::from_str(r#"{"name":"env","number":10}"#).unwrap();
+        let cli: Config = serde_json::from_str(r#"{"name":"cli"}"#).unwrap();
+
+        let config = Config {
+            name: "config",
+            number: file
+                .number
+                .extend_prior(env.number)
+                .extend_prior(cli.number)
+                .into_single(),
+        };
+
+        assert_eq!(config.number.unwrap(), 10);
+    }
 }
