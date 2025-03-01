5b7d4b964ee28822aa481dd8ce0f0b116b372766
hayas1 <h4ystack@gmail.com>
2025-03-01 15:19:36 +0000

    FromStr

--- src/clap.rs
+++ src/clap.rs
@@ -1,10 +1,21 @@
-use crate::{Coalesced, Prior};
+use std::str::FromStr;
 
-pub fn parser(s: &str) -> Result<Coalesced<Option<i64>, Prior>, String> {
-    s.parse()
-        .map(Some)
-        .map(Coalesced::new_prior)
-        .map_err(|e: std::num::ParseIntError| e.to_string())
+use crate::Coalesced;
+
+// TODO not Option<T>, T instead ?
+impl<T, P> FromStr for Coalesced<Option<T>, P>
+where
+    T: FromStr,
+{
+    type Err = T::Err;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        if s.is_empty() {
+            Ok(Coalesced::new(None))
+        } else {
+            s.parse().map(Some).map(Coalesced::new)
+        }
+    }
 }
 
 #[cfg(test)]
@@ -17,13 +28,16 @@ mod tests {
     #[clap(version, about, arg_required_else_help = true)]
     pub struct Cli {
         /// Some number
-        #[arg(short, long, value_parser = parser)]
-        pub number: Coalesced<Option<i64>, Prior>,
+        #[arg(short, long)]
+        pub number: Coalesced<Option<i64>>,
     }
 
     #[test]
     fn test_clap_parser() {
         let cli = Cli::try_parse_from(["coalesced", "--number", "100"]).unwrap();
         assert_eq!(cli.number.unwrap(), 100);
+
+        let cli = Cli::try_parse_from(["coalesced"]).unwrap();
+        assert!(cli.number.is_none());
     }
 }
