16761d7060a5294f45415919b0b23d68ec3b22f3
hayas1 <h4ystack@gmail.com>
2025-03-07 18:30:46 +0000

    export

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,6 @@ mod clap;
 #[cfg(feature = "serde")]
 mod serde;
 
-pub use coalesce::{Coalesce, Straight};
+pub use coalesce::{Coalesce, CoalesceExt, Straight};
 pub use coalesced::Coalesced;
 pub use priority::{Multiple, Posterior, Prior, Single};
