cd2b8d9053a159ba8eec5793ac3d021ad8dd6d33
hayas1 <h4ystack@gmail.com>
2025-03-08 06:51:41 +0000

    modify test

--- tests/basic.rs
+++ tests/basic.rs
@@ -1,4 +1,4 @@
-use coalesced::{Coalesce, Coalesced};
+use coalesced::{Coalesce, CoalesceExt};
 
 pub struct Config<'a> {
     pub name: &'a str,
@@ -6,46 +6,47 @@ pub struct Config<'a> {
 
 #[test]
 fn test_coalesced_basic_config() {
-    let from_file = Coalesced::new_prior(Some(Config { name: "file" }));
-    let from_env = Coalesced::new_prior(Some(Config { name: "env" }));
-    let from_cli = Coalesced::new_prior(Some(Config { name: "cli" }));
+    let from_file = Some(Config { name: "file" });
+    let from_env = Some(Config { name: "env" });
+    let from_cli = Some(Config { name: "cli" });
 
-    let config = from_file.coalesce(from_env).coalesce(from_cli);
+    let config = from_file.coalesce(from_env).coalesce(from_cli).prior();
     assert_eq!(config.as_ref().unwrap().name, "cli");
 }
 
 struct GlobalConfig<'a> {
     _name: &'a str,
-    number: Coalesced<Option<i64>>,
+    number: Option<i64>,
     locals: Vec<LocalConfig<'a>>,
 }
 struct LocalConfig<'a> {
     _name: &'a str,
-    number: Coalesced<Option<i64>>,
+    number: Option<i64>,
 }
 #[test]
 fn test_coalesced_complex_config() {
     let config = GlobalConfig {
         _name: "global",
-        number: Coalesced::new_prior(Some(1)),
+        number: Some(1),
         locals: vec![
             LocalConfig {
                 _name: "local1",
-                number: Coalesced::new_prior(Some(10)),
+                number: Some(10),
             },
             LocalConfig {
                 _name: "local2",
-                number: Coalesced::new_prior(Some(100)),
+                number: Some(100),
             },
             LocalConfig {
                 _name: "local3",
-                number: Coalesced::new_prior(None),
+                number: None,
             },
         ],
     };
+    let base = config.number.prior();
     let expected = [Some(10), Some(100), Some(1)];
     for (cfg, exp) in config.locals.into_iter().zip(expected) {
-        let number = config.number.clone().coalesce(cfg.number);
+        let number = base.clone().coalesce(cfg.number);
         assert_eq!(*number, exp);
     }
 }
