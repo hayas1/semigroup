4eb7bc7977f17fae4fbd43eafa70a76dbf2d0944
hayas1 <h4ystack@gmail.com>
2025-03-11 16:54:04 +0000

    derive `Coalesce` trait

--- coalesced_derive/src/lib.rs
+++ coalesced_derive/src/lib.rs
@@ -1,6 +1,17 @@
 use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(Coalesce)]
-pub fn derive(input: TokenStream) -> TokenStream {
-    proc_macro::TokenStream::new()
+pub fn derive_coalesce(input: TokenStream) -> TokenStream {
+    let target = parse_macro_input!(input as DeriveInput);
+    let ident = &target.ident;
+    let expand = quote! {
+        impl coalesced::Coalesce for #ident {
+            fn order(&self, other: &Self) -> std::cmp::Ordering {
+                std::cmp::Ordering::Less
+            }
+        }
+    };
+    proc_macro::TokenStream::from(expand)
 }
--- coalesced_derive/tests/derive.rs
+++ coalesced_derive/tests/derive.rs
@@ -1,16 +1,12 @@
 use coalesced_derive::Coalesce;
 
 #[derive(Coalesce)]
-struct Unit;
-
-#[derive(Coalesce)]
-struct Empty {}
+struct Config {
+    value: Option<u32>,
+}
 
 #[test]
 fn test_derive_compile() {
-    let unit = Unit;
-    assert!(matches!(unit, Unit));
-
-    let empty = Empty {};
-    assert!(matches!(empty, Empty {}));
+    let config = Config { value: None };
+    assert_eq!(config.value, None);
 }
