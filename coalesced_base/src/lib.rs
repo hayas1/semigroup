e7324436dea9ae23d80ef9b68717657635cb2815
hayas1 <h4ystack@gmail.com>
2025-03-19 15:09:21 +0000

    other

--- coalesced_base/src/lib.rs
+++ coalesced_base/src/lib.rs
@@ -12,3 +12,27 @@ impl<T, E> Coalesce for Result<T, E> {
         self.or(other)
     }
 }
+
+pub trait CoalesceOther {}
+impl<T: CoalesceOther> Coalesce for T {
+    fn coalesce(self, other: Self) -> Self {
+        other
+    }
+}
+impl CoalesceOther for () {}
+impl CoalesceOther for bool {}
+impl CoalesceOther for char {}
+impl CoalesceOther for str {}
+impl CoalesceOther for String {}
+impl CoalesceOther for u8 {}
+impl CoalesceOther for u16 {}
+impl CoalesceOther for u32 {}
+impl CoalesceOther for u64 {}
+impl CoalesceOther for u128 {}
+impl CoalesceOther for i8 {}
+impl CoalesceOther for i16 {}
+impl CoalesceOther for i32 {}
+impl CoalesceOther for i64 {}
+impl CoalesceOther for i128 {}
+impl CoalesceOther for f32 {}
+impl CoalesceOther for f64 {}
