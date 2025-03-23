65976c15b67f207ac9f68acaa9d5c554dd27a3a2
hayas1 <h4ystack@gmail.com>
2025-03-23 15:18:45 +0000

    drop

--- coalesced_base/src/extension.rs
+++ coalesced_base/src/extension.rs
@@ -78,10 +78,14 @@ impl<T, X> Coalesce for Extended<Option<T>, X> {
         );
 
         match s.prior(o) {
-            Some((v, x)) => Extended {
-                value: Some(v),
-                extension: Rc::try_unwrap(x).unwrap_or_else(|_| unreachable!()),
-            },
+            Some((v, x)) => {
+                drop(se);
+                drop(oe);
+                Extended {
+                    value: Some(v),
+                    extension: Rc::try_unwrap(x).unwrap_or_else(|_| unreachable!()),
+                }
+            }
             None => Extended {
                 value: None,
                 extension: Rc::try_unwrap(oe).unwrap_or_else(|_| unreachable!()),
@@ -96,10 +100,14 @@ impl<T, X> Coalesce for Extended<Option<T>, X> {
         );
 
         match s.posterior(o) {
-            Some((v, x)) => Extended {
-                value: Some(v),
-                extension: Rc::try_unwrap(x).unwrap_or_else(|_| unreachable!()),
-            },
+            Some((v, x)) => {
+                drop(se);
+                drop(oe);
+                Extended {
+                    value: Some(v),
+                    extension: Rc::try_unwrap(x).unwrap_or_else(|_| unreachable!()),
+                }
+            }
             None => Extended {
                 value: None,
                 extension: Rc::try_unwrap(se).unwrap_or_else(|_| unreachable!()),
@@ -120,6 +128,8 @@ impl<T, E, X> Coalesce for Extended<Result<T, E>, X> {
                 .map(|v| (v, oe.clone()))
                 .map_err(|e| (e, oe.clone())),
         );
+        drop(se);
+        drop(oe);
 
         match s.prior(o) {
             Ok((v, x)) => Extended {
@@ -143,6 +153,8 @@ impl<T, E, X> Coalesce for Extended<Result<T, E>, X> {
                 .map(|v| (v, oe.clone()))
                 .map_err(|e| (e, oe.clone())),
         );
+        drop(se);
+        drop(oe);
 
         match s.posterior(o) {
             Ok((v, x)) => Extended {
@@ -162,7 +174,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_option_extension() {
+    fn test_option_prior_extension() {
         let file = Some(1).with_extension("file");
         let env = Some(10).with_extension("env");
         let cli = None.with_extension("cli");
@@ -171,5 +183,41 @@ mod tests {
         assert_eq!(*coalesced, Some(10));
         assert_eq!(coalesced.unwrap(), 10);
         assert_eq!(coalesced.extension_ref(), &"env");
+    }
+
+    #[test]
+    fn test_option_posterior_extension() {
+        let file = Some(1).with_extension("file");
+        let env = Some(10).with_extension("env");
+        let cli = None.with_extension("cli");
+
+        let coalesced = file.posterior(env).posterior(cli);
+        assert_eq!(*coalesced, Some(1));
+        assert_eq!(coalesced.unwrap(), 1);
+        assert_eq!(coalesced.extension_ref(), &"file");
+    }
+
+    #[test]
+    fn test_result_prior_extension() {
+        let file = Ok(1).with_extension("file");
+        let env = Ok(10).with_extension("env");
+        let cli = Err(1).with_extension("cli");
+
+        let coalesced = file.prior(env).prior(cli);
+        assert_eq!(*coalesced, Ok(10));
+        assert_eq!(coalesced.unwrap(), 10);
+        assert_eq!(coalesced.extension_ref(), &"env");
+    }
+
+    #[test]
+    fn test_result_posterior_extension() {
+        let file = Ok(1).with_extension("file");
+        let env = Ok(10).with_extension("env");
+        let cli = Err(1).with_extension("cli");
+
+        let coalesced = file.posterior(env).posterior(cli);
+        assert_eq!(*coalesced, Ok(1));
+        assert_eq!(coalesced.unwrap(), 1);
+        assert_eq!(coalesced.extension_ref(), &"file");
     }
 }
