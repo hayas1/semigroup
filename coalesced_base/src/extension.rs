fe403a8608397bcd92272bbe0d3841e7395c5a58
hayas1 <h4ystack@gmail.com>
2025-03-23 13:19:24 +0000

    deref

--- coalesced_base/src/extension.rs
+++ coalesced_base/src/extension.rs
@@ -13,7 +13,7 @@ impl<T> Extension<T> for Option<T> {
     type Output<X> = Extended<Option<T>, X>;
     fn with_extension<X>(self, extension: X) -> Self::Output<X> {
         Extended {
-            base: self,
+            value: self,
             extension,
         }
     }
@@ -22,7 +22,7 @@ impl<T, E> Extension<T> for Result<T, E> {
     type Output<X> = Extended<Result<T, E>, X>;
     fn with_extension<X>(self, extension: X) -> Self::Output<X> {
         Extended {
-            base: self,
+            value: self,
             extension,
         }
     }
@@ -30,24 +30,60 @@ impl<T, E> Extension<T> for Result<T, E> {
 
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Default, Hash)]
 pub struct Extended<C, X> {
-    base: C,
+    value: C,
     extension: X,
+}
+impl<C, X> Extended<C, X> {
+    pub fn into(self) -> C {
+        self.value
+    }
+    pub fn into_extension(self) -> X {
+        self.extension
+    }
+    pub fn into_parts(self) -> (C, X) {
+        (self.value, self.extension)
+    }
+
+    pub fn value_ref(&self) -> &C {
+        &self.value
+    }
+    pub fn extension_ref(&self) -> &X {
+        &self.extension
+    }
+
+    pub fn value_mut(&mut self) -> &mut C {
+        &mut self.value
+    }
+    pub fn extension_mut(&mut self) -> &mut X {
+        &mut self.extension
+    }
+}
+impl<C, X> Deref for Extended<C, X> {
+    type Target = C;
+    fn deref(&self) -> &Self::Target {
+        self.value_ref()
+    }
+}
+impl<C, X> DerefMut for Extended<C, X> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        self.value_mut()
+    }
 }
 impl<T, X> Coalesce for Extended<Option<T>, X> {
     fn prior(self, other: Self) -> Self {
         let (se, oe) = (Rc::new(self.extension), Rc::new(other.extension));
         let (s, o) = (
-            self.base.map(|v| (v, se.clone())),
-            other.base.map(|v| (v, oe.clone())),
+            self.value.map(|v| (v, se.clone())),
+            other.value.map(|v| (v, oe.clone())),
         );
 
         match s.prior(o) {
             Some((v, x)) => Extended {
-                base: Some(v),
+                value: Some(v),
                 extension: Rc::try_unwrap(x).unwrap_or_else(|_| unreachable!()),
             },
             None => Extended {
-                base: None,
+                value: None,
                 extension: Rc::try_unwrap(oe).unwrap_or_else(|_| unreachable!()),
             },
         }
@@ -55,31 +91,20 @@ impl<T, X> Coalesce for Extended<Option<T>, X> {
     fn posterior(self, other: Self) -> Self {
         let (se, oe) = (Rc::new(self.extension), Rc::new(other.extension));
         let (s, o) = (
-            self.base.map(|v| (v, se.clone())),
-            other.base.map(|v| (v, oe.clone())),
+            self.value.map(|v| (v, se.clone())),
+            other.value.map(|v| (v, oe.clone())),
         );
 
         match s.posterior(o) {
             Some((v, x)) => Extended {
-                base: Some(v),
+                value: Some(v),
                 extension: Rc::try_unwrap(x).unwrap_or_else(|_| unreachable!()),
             },
             None => Extended {
-                base: None,
+                value: None,
                 extension: Rc::try_unwrap(se).unwrap_or_else(|_| unreachable!()),
             },
         }
-    }
-}
-impl<T, X> Deref for Extended<Option<T>, X> {
-    type Target = Option<T>;
-    fn deref(&self) -> &Self::Target {
-        &self.base
-    }
-}
-impl<T, X> DerefMut for Extended<Option<T>, X> {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.base
     }
 }
 
@@ -87,22 +112,22 @@ impl<T, E, X> Coalesce for Extended<Result<T, E>, X> {
     fn prior(self, other: Self) -> Self {
         let (se, oe) = (Rc::new(self.extension), Rc::new(other.extension));
         let (s, o) = (
-            self.base
+            self.value
                 .map(|v| (v, se.clone()))
                 .map_err(|e| (e, se.clone())),
             other
-                .base
+                .value
                 .map(|v| (v, oe.clone()))
                 .map_err(|e| (e, oe.clone())),
         );
 
         match s.prior(o) {
             Ok((v, x)) => Extended {
-                base: Ok(v),
+                value: Ok(v),
                 extension: Rc::try_unwrap(x).unwrap_or_else(|_| unreachable!()),
             },
             Err((e, x)) => Extended {
-                base: Err(e),
+                value: Err(e),
                 extension: Rc::try_unwrap(x).unwrap_or_else(|_| unreachable!()),
             },
         }
@@ -110,35 +135,41 @@ impl<T, E, X> Coalesce for Extended<Result<T, E>, X> {
     fn posterior(self, other: Self) -> Self {
         let (se, oe) = (Rc::new(self.extension), Rc::new(other.extension));
         let (s, o) = (
-            self.base
+            self.value
                 .map(|v| (v, se.clone()))
                 .map_err(|e| (e, se.clone())),
             other
-                .base
+                .value
                 .map(|v| (v, oe.clone()))
                 .map_err(|e| (e, oe.clone())),
         );
 
         match s.posterior(o) {
             Ok((v, x)) => Extended {
-                base: Ok(v),
+                value: Ok(v),
                 extension: Rc::try_unwrap(x).unwrap_or_else(|_| unreachable!()),
             },
             Err((e, x)) => Extended {
-                base: Err(e),
+                value: Err(e),
                 extension: Rc::try_unwrap(x).unwrap_or_else(|_| unreachable!()),
             },
         }
     }
 }
-impl<T, E, X> Deref for Extended<Result<T, E>, X> {
-    type Target = Result<T, E>;
-    fn deref(&self) -> &Self::Target {
-        &self.base
-    }
-}
-impl<T, E, X> DerefMut for Extended<Result<T, E>, X> {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.base
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_option_extension() {
+        let file = Some(1).with_extension("file");
+        let env = Some(10).with_extension("env");
+        let cli = None.with_extension("cli");
+
+        let coalesced = file.prior(env).prior(cli);
+        assert_eq!(*coalesced, Some(10));
+        assert_eq!(coalesced.unwrap(), 10);
+        assert_eq!(coalesced.extension_ref(), &"env");
     }
 }
