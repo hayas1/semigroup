8fafd0eec880cbe6a6cd832dc69bd76750224375
hayas1 <h4ystack@gmail.com>
2025-03-24 16:45:13 +0000

    use syn::Type

--- coalesced_base/build.rs
+++ coalesced_base/build.rs
@@ -3,35 +3,40 @@ use quote::quote;
 use std::fs;
 use std::path::Path;
 use std::process::Command;
-use std::str::FromStr;
+use syn::{parse_quote, Type};
 
 const GENERATED_RS: &str = "src/generated.rs";
 const RUSTFMT: &str = "rustfmt";
-const EXTENSIONS_PRIMITIVE: &[&str] = &[
-    "()", "bool", "char", "String", "u8", "u16", "u32", "u64", "u128", "i8", "i16", "i32", "i64",
-    "i128", "f32", "f64",
-];
-const EXTENSIONS_REF: &[&str] = &["&str"];
 
 fn main() {
-    let extensions_primitive = EXTENSIONS_PRIMITIVE
+    let primitives = vec![
+        parse_quote! {()},
+        parse_quote! {bool},
+        parse_quote! {char},
+        parse_quote! {String},
+        parse_quote! {u8},
+        parse_quote! {u16},
+        parse_quote! {u32},
+        parse_quote! {u64},
+        parse_quote! {u128},
+        parse_quote! {i8},
+        parse_quote! {i16},
+        parse_quote! {i32},
+        parse_quote! {i64},
+        parse_quote! {i128},
+        parse_quote! {f32},
+        parse_quote! {f64},
+    ];
+    let extensions_primitive = primitives.iter().map(coalesce_extension_impl_type);
+
+    let extensions_primitive_ref = primitives
         .iter()
-        .cloned()
-        .map(TokenStream::from_str)
-        .map(Result::unwrap)
-        .map(coalesce_extension_impl);
-    let extensions_primitive_ref = EXTENSIONS_PRIMITIVE
-        .iter()
-        .map(|s| format!("&{}", s))
-        .map(|s| TokenStream::from_str(&s))
-        .map(Result::unwrap)
-        .map(coalesce_extension_impl);
-    let extensions_ref = EXTENSIONS_REF
-        .iter()
-        .cloned()
-        .map(TokenStream::from_str)
-        .map(Result::unwrap)
-        .map(coalesce_extension_impl);
+        .map(|t| Type::Reference(parse_quote! {&#t}))
+        .map(coalesce_extension_impl_type_owned);
+
+    let extensions_ref = vec![parse_quote!(&str)]
+        .into_iter()
+        .map(coalesce_extension_impl_type_owned);
 
     let generated_impl = quote! {
         use crate::extension::{Extension, WithExt};
@@ -45,7 +50,7 @@ fn main() {
     Command::new(RUSTFMT).arg(&generated_rs).status().unwrap();
 }
 
-fn coalesce_extension_impl(ident: TokenStream) -> TokenStream {
+fn coalesce_extension_impl_type(ident: &Type) -> TokenStream {
     quote! {
         #[doc = "Generated implementation"]
         impl Extension for #ident {
@@ -57,4 +62,7 @@ fn coalesce_extension_impl(ident: TokenStream) -> TokenStream {
             }
         }
     }
+}
+fn coalesce_extension_impl_type_owned(ident: Type) -> TokenStream {
+    coalesce_extension_impl_type(&ident)
 }
