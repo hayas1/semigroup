91895f95fb0ff0d643c05ededdcd5576af10ed80
hayas1 <h4ystack@gmail.com>
2025-03-27 10:04:37 +0000

    method

--- coalesced_derive/src/coalesce.rs
+++ coalesced_derive/src/coalesce.rs
@@ -1,8 +1,22 @@
 use proc_macro2::TokenStream;
-use quote::quote;
-use syn::{Data, DataStruct, DeriveInput, Fields, FieldsNamed, FieldsUnnamed};
+use quote::{quote, ToTokens};
+use syn::{parse_quote, Data, DataStruct, DeriveInput, Fields, FieldsNamed, FieldsUnnamed, Ident};
 
 use crate::error::DeriveError;
+
+enum Method {
+    Prior,
+    Posterior,
+}
+impl ToTokens for Method {
+    fn to_tokens(&self, tokens: &mut TokenStream) {
+        let ident: Ident = match self {
+            Method::Prior => parse_quote! { prior },
+            Method::Posterior => parse_quote! { posterior },
+        };
+        tokens.extend(ident.into_token_stream())
+    }
+}
 
 pub struct CoalesceImplementor {
     input: DeriveInput,
@@ -31,8 +45,8 @@ impl CoalesceImplementor {
         let (g_impl, g_type, g_where) = generics.split_for_impl();
 
         let (prior, posterior) = (
-            self.implement_struct_prior(s),
-            self.implement_struct_posterior(s),
+            self.snippet_struct(s, Method::Prior),
+            self.snippet_struct(s, Method::Posterior),
         );
         quote! {
             impl #g_impl ::coalesced::Coalesce for #ident #g_type #g_where {
@@ -46,16 +60,16 @@ impl CoalesceImplementor {
         }
     }
 
-    fn implement_struct_prior(&self, s: &DataStruct) -> TokenStream {
+    fn snippet_struct(&self, s: &DataStruct, p: Method) -> TokenStream {
         match &s.fields {
             Fields::Named(f) => {
-                let snippet = self.implement_named_prior_snippet(f);
+                let snippet = self.snippet_fields_named(f, p);
                 quote! {
                     Self { #snippet }
                 }
             }
             Fields::Unnamed(f) => {
-                let snippet = self.implement_unnamed_prior_snippet(f);
+                let snippet = self.snippet_fields_unnamed(f, p);
                 quote! {
                     Self( #snippet )
                 }
@@ -66,49 +80,16 @@ impl CoalesceImplementor {
             },
         }
     }
-    fn implement_struct_posterior(&self, s: &DataStruct) -> TokenStream {
-        match &s.fields {
-            Fields::Named(f) => {
-                let snippet = self.implement_named_posterior_snippet(f);
-                quote! {
-                    Self { #snippet }
-                }
-            }
-            Fields::Unnamed(f) => {
-                let snippet = self.implement_unnamed_posterior_snippet(f);
-                quote! {
-                    Self( #snippet )
-                }
-            }
-            Fields::Unit => quote! {
-                let _ = other;
-                self
-            },
-        }
-    }
-    fn implement_named_prior_snippet(&self, f: &FieldsNamed) -> TokenStream {
+    fn snippet_fields_named(&self, f: &FieldsNamed, p: Method) -> TokenStream {
         let fields: Vec<_> = f.named.iter().map(|f| &f.ident).collect();
         quote! {
-            #(#fields: self.#fields.prior(other.#fields)),*
+            #(#fields: self.#fields.#p(other.#fields)),*
         }
     }
-    fn implement_named_posterior_snippet(&self, f: &FieldsNamed) -> TokenStream {
-        let fields: Vec<_> = f.named.iter().map(|f| &f.ident).collect();
-        quote! {
-            #(#fields: self.#fields.prior(other.#fields)),*
-        }
-    }
-
-    fn implement_unnamed_prior_snippet(&self, f: &FieldsUnnamed) -> TokenStream {
+    fn snippet_fields_unnamed(&self, f: &FieldsUnnamed, p: Method) -> TokenStream {
         let enumerates: Vec<_> = (0..f.unnamed.len()).map(syn::Index::from).collect();
         quote! {
-            #(self.#enumerates.prior(other.#enumerates)),*
-        }
-    }
-    fn implement_unnamed_posterior_snippet(&self, f: &FieldsUnnamed) -> TokenStream {
-        let enumerates: Vec<_> = (0..f.unnamed.len()).map(syn::Index::from).collect();
-        quote! {
-            #(self.#enumerates.posterior(other.#enumerates)),*
+            #(self.#enumerates.#p(other.#enumerates)),*
         }
     }
 }
