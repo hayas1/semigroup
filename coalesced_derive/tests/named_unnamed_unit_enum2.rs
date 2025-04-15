502fa0bd678ef567471c91b9c56dbb2bb09ef82d
hayas1 <h4ystack@gmail.com>
2025-04-15 15:47:24 +0000

    test for enum

--- coalesced_derive/tests/named_unnamed_unit_enum2.rs
+++ coalesced_derive/tests/named_unnamed_unit_enum2.rs
@@ -1,6 +1,7 @@
 use coalesced::Coalesce;
 
 use coalesced::Extension;
+use coalesced::WithExt;
 use coalesced_derive::Extension;
 
 #[derive(Extension)]
@@ -35,4 +36,57 @@ fn test_derive_coalesce_enum_unnamed() {
 
     let c = config.prior(config2);
     assert!(matches!(c, Config::Unnamed("op")));
+}
+
+#[test]
+fn test_derive_extension_enum_unit() {
+    let config = Config::Unit.with_extension("first");
+    let config2 = Config::Unit.with_extension("second");
+
+    let c = config.posterior(config2);
+    assert!(matches!(
+        c,
+        // TODO <Config as Extension<_>>::WithExt::Unit, but experimental
+        ConfigWithExt::Unit(WithExt {
+            value: (),
+            extension: "first"
+        })
+    ));
+    assert!(matches!(c.into(), Config::Unit));
+}
+
+#[test]
+fn test_derive_extension_enum_named() {
+    let config = Config::Named { value: 10 }.with_extension("first");
+    let config2 = Config::Named { value: 100 }.with_extension("second");
+
+    let c = config.posterior(config2);
+    assert!(matches!(
+        c,
+        // TODO <Config as Extension<_>>::WithExt::Unit, but experimental
+        ConfigWithExt::Named {
+            value: WithExt {
+                value: 10,
+                extension: "first"
+            }
+        }
+    ));
+    assert!(matches!(c.into(), Config::Named { value: 10 }));
+}
+
+#[test]
+fn test_derive_extension_enum_unnamed() {
+    let config = Config::Unnamed("nop").with_extension("first");
+    let config2 = Config::Unnamed("op").with_extension("second");
+
+    let c = config.posterior(config2);
+    assert!(matches!(
+        c,
+        // TODO <Config as Extension<_>>::WithExt::Unit, but experimental
+        ConfigWithExt::Unnamed(WithExt {
+            value: "nop",
+            extension: "first"
+        })
+    ));
+    assert!(matches!(c.into(), Config::Unnamed("nop")));
 }
