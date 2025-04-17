947cb4b9f1ed4cae9033e2f4a038a424b21a4d1d
hayas1 <h4ystack@gmail.com>
2025-04-17 12:54:32 +0000

    remove old implementation

--- coalesced_derive/src/lib.rs
+++ coalesced_derive/src/lib.rs
@@ -1,15 +1,7 @@
-mod coalesce;
 mod error;
 mod extension;
 
 use proc_macro::TokenStream;
-
-// #[proc_macro_derive(Coalesce)]
-// pub fn derive_coalesce(input: TokenStream) -> TokenStream {
-//     let derive_input = syn::parse_macro_input!(input);
-//     let implementor = coalesce::Implementor::new(derive_input);
-//     implementor.implement().into()
-// }
 
 #[proc_macro_derive(Coalesce)]
 pub fn derive_extension(input: TokenStream) -> TokenStream {
