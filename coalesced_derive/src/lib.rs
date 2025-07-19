71bab25a62cc56cc0c59d815a13e8e6b903fe763
hayas1 <h4ystack@gmail.com>
2025-07-19 07:56:03 +0000

    attribute

--- coalesced_derive/src/lib.rs
+++ coalesced_derive/src/lib.rs
@@ -3,7 +3,7 @@ mod extension;
 
 use proc_macro::TokenStream;
 
-#[proc_macro_derive(Coalesce)]
+#[proc_macro_derive(Coalesce, attributes(coalesced))]
 pub fn derive_extension(input: TokenStream) -> TokenStream {
     let derive_input = syn::parse_macro_input!(input);
     let implementor = extension::Implementor::new(derive_input);
