e768fe271144d42aef62cb9b221a9dd38da5246c
hayas1 <h4ystack@gmail.com>
2025-07-19 17:37:58 +0000

    inert attributes

---
+++ coalesced_derive/tests/named_fields_struct_inert_attributes.rs
@@ -0,0 +1,52 @@
+use coalesced::{Coalesce, Extension};
+
+#[derive(Coalesce)]
+struct Config {
+    #[coalesced(with = "coalesced::overwrite")]
+    name: &'static str,
+    value: Option<u32>,
+}
+
+#[test]
+fn test_derive_coalesce_named_fields_struct_inert_attributes() {
+    let config = Config {
+        name: "c1",
+        value: Some(1),
+    };
+    let config2 = Config {
+        name: "c2",
+        value: None,
+    };
+
+    let c = config.prior(config2);
+    assert_eq!(c.name, "c2");
+    assert_eq!(c.value, Some(1));
+}
+
+#[test]
+fn test_derive_extension_named_fields_struct_inert_attributes() {
+    let config = Config {
+        name: "c1",
+        value: None,
+    }
+    .with_extension("first");
+    let config2 = Config {
+        name: "c2",
+        value: Some(2),
+    }
+    .with_extension("second");
+
+    let c = config.posterior(config2);
+    assert_eq!(c.name.extension, "first");
+    assert_eq!(*c.name, "c1");
+    assert_eq!(c.value.extension, "second");
+    assert_eq!(*c.value, Some(2));
+
+    assert!(matches!(
+        c.into(),
+        Config {
+            name: "c1",
+            value: Some(2),
+        }
+    ));
+}
