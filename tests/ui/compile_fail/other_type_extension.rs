70e46ecd5a601a9ffe1f07c92960662ff4123a1c
hayas1 <h4ystack@gmail.com>
2025-08-17 08:33:32 +0000

    modify ui test

--- tests/ui/compile_fail/other_type_extension.rs
+++ tests/ui/compile_fail/other_type_extension.rs
@@ -1,10 +1,10 @@
-use coalesced::{Coalesce, Extension};
+use coalesced::{annotate::Annotate, op::annotation::coalesce::Coalesce};
 
 fn main() {
-    let a = Some(1).with_extension("a");
-    let b = Some(2).with_extension("b".to_string());
+    let a = Some(1).annotated("a");
+    let b = Some(2).annotated("b".to_string());
 
     let c = a.coalesce(b);
     assert_eq!(c.value, Some(2));
-    assert_eq!(c.extension, "b".to_string());
+    assert_eq!(c.annotation, "b".to_string());
 }
