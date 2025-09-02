e2e2d7b6b630fdf170d05950d6b7c592795d54e2
hayas1 <h4ystack@gmail.com>
2025-09-02 13:19:46 +0000

    AnnotatedSemigroup

--- coalesced_derive/src/constant.rs
+++ coalesced_derive/src/constant.rs
@@ -6,8 +6,9 @@ pub const DERIVE_SEMIGROUP: &str = "Semigroup";
 #[derive(Debug, Clone)]
 pub struct Constant {
     pub path_semigroup: Path,
-    pub _path_annotated_semigroup: Path,
     pub ident_semigroup_op: Ident,
+    pub path_annotated_semigroup: Path,
+    pub ident_annotated_op: Ident,
     pub path_annotated: Path,
     pub path_reversed: Path,
     pub path_construction_trait: Path,
@@ -21,8 +22,9 @@ impl ConstantExt for Absolute {
     fn constant() -> Constant {
         Constant {
             path_semigroup: parse_quote!(::coalesced::Semigroup),
-            _path_annotated_semigroup: parse_quote!(::coalesced::AnnotatedSemigroup),
             ident_semigroup_op: parse_quote!(semigroup_op),
+            path_annotated_semigroup: parse_quote!(::coalesced::AnnotatedSemigroup),
+            ident_annotated_op: parse_quote!(annotated_op),
             path_annotated: parse_quote!(::coalesced::Annotated),
             path_reversed: parse_quote!(::coalesced::Reversed),
             path_construction_trait: parse_quote!(::coalesced::op::Construction),
@@ -37,8 +39,9 @@ impl ConstantExt for Use {
     fn constant() -> Constant {
         Constant {
             path_semigroup: parse_quote!(Semigroup),
-            _path_annotated_semigroup: parse_quote!(AnnotatedSemigroup),
             ident_semigroup_op: parse_quote!(semigroup_op),
+            path_annotated_semigroup: parse_quote!(AnnotatedSemigroup),
+            ident_annotated_op: parse_quote!(annotated_op),
             path_annotated: parse_quote!(Annotated),
             path_reversed: parse_quote!(Reversed),
             path_construction_trait: parse_quote!(Construction),
