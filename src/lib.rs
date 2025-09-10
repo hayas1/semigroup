874f79ca21f30354bd4d58f3ef911426b13a0686
hayas1 <h4ystack@gmail.com>
2025-09-10 15:47:39 +0000

    wip: doc

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,28 @@
+//! coalesced supports reading configs from multiple sources
+//!
+//! # Usage
+//! ```toml
+//! [dependencies]
+//! coalesced = { git = "https://github.com/hayas1/coalesced" }
+//! ```
+//!
+//! # Examples
+//!
+//! ## Annotation
+//!
+//! ## Lazy Evaluation
+//!
+//! # Documents
+//! <https://hayas1.github.io/coalesced/coalesced>
+//!
+//! # Testing
+//! ## Benchmarks
+//! // TODO
+//!
+//! ## Coverage
+//! <https://hayas1.github.io/coalesced/coalesced/tarpaulin-report.html>
+//!
+
 pub use coalesced_base::{
     annotate::{Annotate, Annotated},
     lazy::LazySemigroup,
