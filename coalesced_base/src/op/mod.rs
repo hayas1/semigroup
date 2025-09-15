9e87bf43a500e9cb33388d2d60fc968598f2adc2
hayas1 <h4ystack@gmail.com>
2025-09-15 10:55:20 +0000

    traits

--- coalesced_base/src/op/mod.rs
+++ coalesced_base/src/op/mod.rs
@@ -1,3 +1,5 @@
+use std::ops::{Deref, DerefMut};
+
 use crate::{
     annotate::{Annotate, Annotated},
     semigroup::{AnnotatedSemigroup, Semigroup},
@@ -6,11 +8,11 @@ use crate::{
 pub mod annotation;
 pub mod semigroup;
 
-pub trait Construction<T>: Sized + Semigroup {
+pub trait Construction<T>: Semigroup + Sized + From<T> + Deref<Target = T> + DerefMut {
     fn new(value: T) -> Self;
     fn into_inner(self) -> T;
     fn lift_op(base: T, other: T) -> T {
-        Semigroup::semigroup_op(Self::new(base), Self::new(other)).into_inner()
+        Semigroup::semigroup_op(Self::from(base), Self::from(other)).into_inner()
     }
 }
 
@@ -18,7 +20,7 @@ pub trait ConstructionAnnotated<T, A>:
     Construction<T> + AnnotatedSemigroup<A> + Annotate<A>
 {
     fn lift_annotated_op(base: Annotated<T, A>, other: Annotated<T, A>) -> Annotated<T, A> {
-        AnnotatedSemigroup::annotated_op(base.map(Self::new), other.map(Self::new))
+        AnnotatedSemigroup::annotated_op(base.map(Self::from), other.map(Self::from))
             .map(Self::into_inner)
     }
 }
