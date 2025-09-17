28615741c879af4ce056ce00d194b2acb833a3ac
hayas1 <h4ystack@gmail.com>
2025-09-17 13:03:56 +0000

    refactor lazy

--- coalesced_base/src/lazy.rs
+++ coalesced_base/src/lazy.rs
@@ -1,56 +1,72 @@
-use std::ops::{Deref, DerefMut};
-
 use crate::semigroup::Semigroup;
 
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Default, Hash)]
-pub struct LazySemigroup<T>(Vec<T>);
+pub struct LazySemigroup<T> {
+    head: T,
+    tail: Vec<T>,
+}
 impl<T> Semigroup for LazySemigroup<T> {
     fn semigroup_op(mut base: Self, other: Self) -> Self {
         base.extend(other);
         base
     }
 }
+impl<T: Semigroup> LazySemigroup<T> {
+    pub fn evaluate(self) -> T {
+        let Self { head, tail } = self;
+        tail.into_iter()
+            .fold(head, |acc, item| T::semigroup_op(acc, item))
+    }
+    pub fn evaluate_cloned(&self) -> T
+    where
+        T: Clone,
+    {
+        let Self { head, tail } = self;
+        tail.iter()
+            .cloned()
+            .fold(head.clone(), |acc, item| T::semigroup_op(acc, item))
+    }
+}
 
 impl<T> LazySemigroup<T> {
     pub fn with(t: T) -> Self {
-        Self(vec![t])
-    }
-    pub fn evaluate(self) -> T
-    where
-        T: Semigroup,
-    {
-        let mut iter = self.into_iter();
-        let base = iter.next().unwrap_or_else(|| unreachable!());
-        iter.fold(base, |acc, item| T::semigroup_op(acc, item))
-    }
-}
-
-impl<T> Deref for LazySemigroup<T> {
-    type Target = Vec<T>;
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
-impl<T> DerefMut for LazySemigroup<T> {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.0
-    }
-}
-impl<T> From<LazySemigroup<T>> for Vec<T> {
-    fn from(value: LazySemigroup<T>) -> Self {
-        value.0
+        Self {
+            head: t,
+            tail: Vec::new(),
+        }
     }
 }
 impl<T> IntoIterator for LazySemigroup<T> {
     type Item = T;
-    type IntoIter = std::vec::IntoIter<T>;
+    type IntoIter = IntoIter<T>;
 
     fn into_iter(self) -> Self::IntoIter {
-        self.0.into_iter()
+        IntoIter {
+            head: Some(self.head),
+            tail: self.tail.into_iter(),
+        }
     }
 }
 impl<T> Extend<T> for LazySemigroup<T> {
     fn extend<I: IntoIterator<Item = T>>(&mut self, iter: I) {
-        self.0.extend(iter);
+        self.tail.extend(iter);
+    }
+}
+
+#[derive(Debug, Clone, Default)]
+pub struct IntoIter<T> {
+    head: Option<T>,
+    tail: <Vec<T> as IntoIterator>::IntoIter,
+}
+impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+
+    #[inline]
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.head.is_some() {
+            self.head.take()
+        } else {
+            self.tail.next()
+        }
     }
 }
