69b45090f9ef96c75038e65bb94e80e694d41226
hayas1 <h4ystack@gmail.com>
2025-10-01 14:22:00 +0000

    feature

--- tests/compile.rs
+++ tests/compile.rs
@@ -1,5 +1,27 @@
+use std::path::Path;
+
 #[test]
 fn test_ui() {
     let t = trybuild::TestCases::new();
-    t.compile_fail("tests/ui/compile_fail/**/*.rs");
+    #[cfg(not(feature = "monoid"))]
+    {
+        prepare_rs_file("tests/ui/compile_fail", "tests/ui/semigroup").unwrap();
+        t.compile_fail("tests/ui/semigroup/**/*.rs");
+    }
+    #[cfg(feature = "monoid")]
+    {
+        prepare_rs_file("tests/ui/compile_fail", "tests/ui/monoid").unwrap();
+        t.compile_fail("tests/ui/monoid/**/*.rs");
+    }
+}
+
+fn prepare_rs_file<P: AsRef<Path>>(source: P, target: P) -> Result<(), std::io::Error> {
+    let files = std::fs::read_dir(source.as_ref())?;
+    files
+        .filter_map(Result::ok)
+        .filter(|f| f.path().extension().map(|e| e == "rs").unwrap_or(false))
+        .try_for_each(|f| {
+            let t = target.as_ref().join(f.file_name());
+            std::fs::copy(f.path(), &t).map(|_| ())
+        })
 }
