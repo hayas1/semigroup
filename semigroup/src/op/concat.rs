7c3d2f659b3c650fd9ef11e9728d8e3274919096
hayas1 <h4ystack@gmail.com>
2025-11-04 13:25:01 +0000

    annotation where

--- semigroup/src/op/concat.rs
+++ semigroup/src/op/concat.rs
@@ -26,7 +26,11 @@ use crate::{Annotated, AnnotatedSemigroup};
     unit_annotation = "vec![(); 0]",
     without_annotate_impl
 )]
-#[properties_priv(annotated, monoid, annotation_where = "A::Item: Clone")]
+#[properties_priv(
+    annotated,
+    monoid,
+    annotation_where = "A: IntoIterator + FromIterator<A::Item>"
+)]
 pub struct Concat<T: IntoIterator + FromIterator<T::Item>>(pub T);
 impl<T: IntoIterator + FromIterator<T::Item>, A: IntoIterator + FromIterator<A::Item>>
     AnnotatedSemigroup<A> for Concat<T>
